b077ca7761e9ee517165af51d48b5952d7e615c2
sirflano <sirflano@gmail.com>
2015-10-14 15:34:58 +0000

    I had it wrong, fixed it

--- lab5/my_application/server.py
+++ lab5/my_application/server.py
@@ -7,15 +7,17 @@ def index():
 
 @app.route("/hello")
 def hello():
-    return "Hello World"  
+    return "Hello World"
 
-@app.route("/user/paul")
-def paul():
-    return "User paul"
-  
-@app.route("/post/80")
-def post80():
-    return "Post 80"
-  
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0',	port=8080,debug=True)
