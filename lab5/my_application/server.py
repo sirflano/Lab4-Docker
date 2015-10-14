69fa4bc19956f97f08779bfe918430035d3b11bb
sirflano <sirflano@gmail.com>
2015-10-14 15:19:49 +0000

    added new def's to server.py

--- lab5/my_application/server.py
+++ lab5/my_application/server.py
@@ -2,8 +2,20 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    return "Index"
 
+@app.route("/hello")
+def hello():
+    return "Hello World"  
+
+@app.route("/user/paul")
+def paul():
+    return "User paul"
+  
+@app.route("/post/80")
+def post80():
+    return "Post 80"
+  
 if __name__ == "__main__":
-    	app.run(host='0.0.0.0',	port=8080,debug=True)
+    app.run(host='0.0.0.0',	port=8080,debug=True)
